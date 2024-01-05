9e4f93fc79bb448aac5d983cca3bd9067ceff2dc
Nick Howell <nlhowell12@gmail.com>
2024-01-05 12:17:44 -0500

    title updated on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@
 import { Grid } from '@mui/material';
 import { CharacterCardList } from './_components/CharacterCardList';
 import useCharacterService from './api/_services/useCharacterService';
-import { UserSignIn } from './_components/UserSignIn';
-import { useContext } from 'react';
-import UserContext from './_auth/UserContext';
+import { useEffect } from 'react';
 
 export default function Home() {
     const { characters } = useCharacterService();
-    const { user } = useContext(UserContext);
 
+    useEffect(() => {
+        document.title = 'Rhedrah Character Sheet'
+    }, [])
+    
     return (
         <Grid container>
             {!!characters && <CharacterCardList characters={characters} />}
