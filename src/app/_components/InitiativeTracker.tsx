31998190c3e21e62e4ab877aede702fbe72d4008
Nicolas Howell <nlhowell12@gmail.com>
2025-01-23 19:56:37 -0500

    v1 turn tracker complete

--- src/app/_components/InitiativeTracker.tsx
+++ src/app/_components/InitiativeTracker.tsx
@@ -30,6 +30,7 @@ export const InitiativeTracker = () => {
     const [messages, updateMessages] = useState<TrackerMessage[]>([]);
     const [name, setName] = useState('');
     const [score, setScore] = useState(0);
+    const [turn, setTurn] = useState(1);
     const { channel } = useChannel('init-tracker', (message) => {
         const messageIndex = R.findIndex(R.propEq(message.name, 'name'))(
             messages
@@ -39,7 +40,7 @@ export const InitiativeTracker = () => {
         } else if(message.name === 'tracker-delete') {
             handleDelete(message)
         } else if(message.name === 'tracker-clear') {
-            handleClear(message);
+            updateMessages([]);
         } else {
             updateMessages(
                 R.update(messageIndex, message as TrackerMessage, messages)
@@ -47,9 +48,16 @@ export const InitiativeTracker = () => {
         }
     });
 
+    const { channel: turnChannel } = useChannel('turn-tracker', (message) => {
+        setTurn(message.data.value);
+    });
+
     const getTrackerHistory = async () => {
         const messages = await channel.history();
+        const turns = await turnChannel.history();
         const sortedFilteredMessages: TrackerMessage[] = [];
+        console.log(messages)
+        const lastTrackerClearTimestamp = R.last(messages.items.sort((a, b) => a.timestamp - b.timestamp).filter(x => x.name === 'tracker-clear'))?.timestamp;
         messages.items.forEach((m) => {
             const messageIndex = R.findIndex(R.propEq(m.name, 'name'))(
                 sortedFilteredMessages
@@ -60,18 +68,30 @@ export const InitiativeTracker = () => {
                         .filter((x) => x.name === m.name)
                         .sort((a, b) => a.timestamp - b.timestamp)
                 );
-                sortedFilteredMessages.push(lastMessage as TrackerMessage);
+                if(!!lastTrackerClearTimestamp && !!lastMessage && (lastMessage.timestamp > lastTrackerClearTimestamp)){
+                    sortedFilteredMessages.push(lastMessage as TrackerMessage);
+                }
             }
         });
         updateMessages(sortedFilteredMessages);
+        const lastTurn = R.last(turns.items.sort((a, b) => a.timestamp - b.timestamp))
+        !!lastTurn && setTurn(lastTurn.data.value);
     };
 
+    const updateTurn = () => {
+        const newTurn = turn + 1;
+        setTurn(newTurn)
+        turnChannel.publish('turn-update', {value: newTurn, delete: true})
+    }
     const handleDelete = (m: TrackerMessage) => {
         const filter = (x: TrackerMessage) => x.id !== m.data.id;
         updateMessages(R.filter(filter, messages))
     };
-    const handleClear = (m: TrackerMessage) => {
+    const handleClear = () => {
         updateMessages([]);
+        channel.publish('tracker-clear', {data: {value: 0, delete: true}, id: uuidv4(), extras: {ref: {type: 'tracker-clear'}}}) 
+        setTurn(1);
+        turnChannel.publish('turn-update', {value: 1})
     };
 
     useEffect(() => {
@@ -81,7 +101,17 @@ export const InitiativeTracker = () => {
     return (
         <Card>
             <CardHeader title='Initiative Tracker' />
-            <CardContent sx={{ display: 'flex' }}>
+           
+            <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{display: 'flex', alignItems: 'center', marginBottom: '1rem'}}>
+                <h2 style={{fontSize: '1.5rem'}}>Turn: {turn}</h2>
+                <Button
+                    onClick={() => updateTurn()}
+                    sx={{marginLeft: '2rem'}}
+                >
+                    Next Turn
+                </Button>
+                </div>
                 <Grid container>
                     <Grid item xs={6}>
                         <TextField
@@ -124,7 +154,7 @@ export const InitiativeTracker = () => {
                 </Grid>
             </CardContent>
             <CardActions sx={{justifyContent: 'flex-end'}}>
-                <Button color='error' onClick={() => channel.publish('tracker-clear', {data: {value: 0, delete: true}, id: uuidv4(), extras: {ref: {type: 'tracker-clear'}}}) }>
+                <Button color='error' onClick={() => handleClear()}>
                     Clear Initiative
                 </Button>
                 <Button onClick={() => channel.publish(name, {value: score, delete: false})}>
